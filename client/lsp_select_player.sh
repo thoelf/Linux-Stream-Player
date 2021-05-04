7c56081c94d9e129457f5258afc02a396de231e2
Thomas Elfström <thomas.elfstrom@gmail.com>
2021-05-04 22:21:30 +0200

    Update lsp_select_player.sh

--- client/lsp_select_player.sh
+++ client/lsp_select_player.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# lsp-select_player.sh
 
 # Read the arguments
 ACTION=$1
@@ -22,8 +23,13 @@ fi
 # Source the settings file
 . $HOME/.lsp/lsp.conf
 
+# Add @ to the MPD password, if not empty
+if ! [[ -z $MPD_PW ]]; then
+    MPD_PW=$MPD_PW@
+fi
+
 # Check if the port for player selection is available
-if ! echo > /dev/tcp/$LSP_SERVER/$PLAYER_SELECT_PORT; then
+if ! echo > /dev/tcp/$SERVER/$PLAYER_SELECT_PORT; then
     notify-send --icon=dialog-warning "AUDIO STREAMING" "The port for player selection on the server is not available."
     exit 1
 fi
@@ -33,8 +39,8 @@ ps -ef | grep vlc | grep null.monitor >/dev/null && STREAMING=True || STREAMING=
 
 # Play with MPD
 if [[ $ACTION = mpd || ($ACTION = toggle && ! $STREAMING) ]]; then
-    # Start MPD and CamillaDSP on the server
-    netcat -q0 $LSP_SERVER $PLAYER_SELECT_PORT echo <<< mpd
+    # Start MPD on the server
+    netcat -q0 $SERVER $PLAYER_SELECT_PORT echo <<< mpd
     echo mpd > $HOME/.lsp/selected_player.txt
     notify-send "AUDIO STREAMING" "Player: MPD"
 
@@ -46,22 +52,23 @@ if [[ $ACTION = mpd || ($ACTION = toggle && ! $STREAMING) ]]; then
         cvlc -q pulse://null.monitor \
         --network-caching=40 \
         --sout "#transcode{\
-        	vcodec=none,\
-        	acodec=flac,\
-        	channels=2}\
-        	:standard{\
-        	access=http,\
-        	name=$STREAM_NAME,\
-        	dst=$LOCAL_IP:$LOCAL_PORT/$STREAM_NAME}" &
+            vcodec=none,\
+            acodec=flac,\
+            channels=2}\
+            :standard{\
+            access=http,\
+            name=$STREAM_NAME,\
+            dst=$CLIENT:$CLIENT_PORT/$STREAM_NAME}" &
         sleep 2
-        mpc --host=$MPD_PW@$LSP_SERVER --port=$MPD_PORT clear >/dev/null
-        mpc --host=$MPD_PW@$LSP_SERVER --port=$MPD_PORT add http://$LOCAL_IP:$LOCAL_PORT/$STREAM_NAME >/dev/null
-        mpc --host=$MPD_PW@$LSP_SERVER --port=$MPD_PORT play >/dev/null
+        mpc --host=$MPD_PW$SERVER --port=$MPD_PORT clear >/dev/null
+        mpc --host=$MPD_PW$SERVER --port=$MPD_PORT add http://$CLIENT:$CLIENT_PORT/$STREAM_NAME >/dev/null
+        mpc --host=$MPD_PW$SERVER --port=$MPD_PORT play >/dev/null
     fi
 # Play with Squeeze
 elif [[ $ACTION = squeeze || ($ACTION = toggle && $STREAMING) ]]; then
-    mpc --host=$MPD_PW@$LSP_SERVER --port=$MPD_PORT stop >/dev/null
-    netcat -q0 $LSP_SERVER $PLAYER_SELECT_PORT echo <<< squeeze
+    mpc --host=$MPD_PW$SERVER --port=$MPD_PORT stop >/dev/null
+    # Start squeeze on the server
+    netcat -q0 $SERVER $PLAYER_SELECT_PORT echo <<< squeeze
     echo squeeze > $HOME/.lsp/selected_player.txt
     notify-send "AUDIO STREAMING" "Player: SqueezeLite"
     [[ $STREAMING ]] && kill $(ps -ef | grep vlc | grep null.monitor | awk '{ print $2 }')
